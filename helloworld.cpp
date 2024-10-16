698913008a9f90508a5b7dbaf4bb7551a61f0072
Nethmina Sandaruwan <125788788+ByteBigBoss@users.noreply.github.com>
2024-10-17 02:50:01 +0530

    updates-0.0.1

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,16 +7,17 @@ using namespace std;
 int main()
 {
     vector<string> msg {
-        "Hello",
-        "C++",
-        "World",
-        "from",
-        "ByteBigBoss!"
+        "\033[31mHello",  // Red
+        "\033[32mC++",    // Green
+        "\033[34mWorld",  // Blue
+        "\033[33mfrom",   // Yellow
+        "\033[35mByteBigBoss!"  // Magenta
     };
 
     for (const string & word: msg)
     {
         cout << word << " ";
     }
-    cout << endl;
+
+    cout << "\033[0m" << endl;  // Reset color to default
 }
